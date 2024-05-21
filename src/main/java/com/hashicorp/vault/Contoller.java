6f34a948f83f948e0eb19be4b405c7ed94997a47
Amol Nakhate <amol64546@gmail.com>
2024-05-21 18:29:20 +0530

    added decrypt method

--- src/main/java/com/hashicorp/vault/Contoller.java
+++ src/main/java/com/hashicorp/vault/Contoller.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,18 @@ public class Contoller {
       .build();
     personRepo.save(p);
     return personRepo.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Person getById(@PathVariable Integer id) {
+    Person person = personRepo.findById(id).get();
+    Person p = Person.builder()
+      .id(person.getId())
+      .name(person.getName())
+      .username(vaultTransit.decrypt(person.getUsername()))
+      .password(vaultTransit.decrypt(person.getPassword()))
+      .build();
+    return p;
   }
 
 
